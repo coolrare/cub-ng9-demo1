5de624214ae455e86220e9fd80ffe51db1df45a3
Will <doggy.huang@gmail.com>
2020-06-08 13:41:07 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cub-ng9-demo1';
+}
