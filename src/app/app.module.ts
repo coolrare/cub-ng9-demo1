dbddb2db6e0eccdc1ac5bbed7e177f6e31945b11
Will <doggy.huang@gmail.com>
2020-06-08 20:43:33 +0800

    建立 MaskPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MaskPipe } from './mask.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MaskPipe
   ],
   imports: [
     BrowserModule,
