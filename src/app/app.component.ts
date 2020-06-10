69dd81747e5a15b7c5a221c724f747f4c690a7c8
Will <doggy.huang@gmail.com>
2020-06-10 14:21:23 +0800

    使用 HttpClient 取得遠端資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,12 @@ export class AppComponent {
   constructor(private datasvc: DataService) { }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.data = this.datasvc.getData();
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.datasvc.getData()
+    .subscribe(data => {
+      this.data = data;
+    });
   }
 
   doSearch(keyword: string) {
