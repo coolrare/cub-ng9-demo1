9b60598c5f8ea54834e3d2bf2bd6eddc0f83dcaf
Will <doggy.huang@gmail.com>
2020-06-10 13:41:37 +0800

    匯入 HttpClientModule 並注入 HttpClient 服務元件

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +7,7 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getData() {
     return [
